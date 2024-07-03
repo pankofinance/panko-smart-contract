8d2d004fff07e1d79be8a873b8bfe7e68886044c
inumune <inumunedate@gmail.com>
2024-07-03 21:51:51 +0700

    remove ifos

--- projects/ifo/contracts/v1/interfaces/IIFOFactoryV1.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-/** @title IIFOFactoryV1.
- * @notice It is an interface for IFOFactoryV1.sol
- */
-interface IIFOFactoryV1 {
-    function privateSignerAddress() external view returns (address);
-}
